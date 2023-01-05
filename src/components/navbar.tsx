0464ad732ab3446b5991429066fb209a87e16a77
Slava <slasla@gmail.com>
2023-01-06 00:10:00 +0200

    css mobile friendly

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,21 +10,25 @@ export const Navbar = () => {
     }
 
     return (
-        <div className="navbar">
-            <img src = {logo} width = "96" height="96" className="logo" alt = "Avatar"/>
-            <div className="links">
-            <Link to = "/">Home</Link>
-            {user ? <Link to = "/createpost">Create Post</Link> :
-            <Link to = "/login">Login</Link>}
-            </div>
-            <div className="user">
-                {user && (
-                    <>
-                        <p>User: {user?.displayName}</p>
-                        <img src = {auth.currentUser?.photoURL || ""} width = "50" height="50" className="img" alt = "Avatar"/>
-                        <button className="logout-button" onClick={logOut}>Log Out</button>
-                    </>
-                )}
+        <div className="navbar-all">
+            <Link to = "/">
+                <img src = {logo} id = "logo" width = "96" height="96" className="logo" alt = "Avatar"/>
+            </Link>
+            <div className="navbar">
+                <div className="links">
+                <Link to = "/">Home</Link>
+                {user ? <Link to = "/createpost">Create Post</Link> :
+                <Link to = "/login">Login</Link>}
+                </div>
+                <div className="user">
+                    {user && (
+                        <>
+                            <p>User: {user?.displayName}</p>
+                            <img src = {auth.currentUser?.photoURL || ""} width = "50" height="50" className="img" alt = "Avatar"/>
+                            <button className="logout-button" onClick={logOut}>Log Out</button>
+                        </>
+                    )}
+                </div>
             </div>
         </div>
     )
